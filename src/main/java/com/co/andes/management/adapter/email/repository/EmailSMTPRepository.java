6fafad6fdbba69682f4e7d4768283956313429fd
Alejandro Gonzalez <wilson1234567891962@hotmail.com>
2023-02-15 08:50:52 -0500

    Create the audit table to have logs

--- src/main/java/com/co/andes/management/adapter/email/repository/EmailSMTPRepository.java
+++ src/main/java/com/co/andes/management/adapter/email/repository/EmailSMTPRepository.java
@@ -75,7 +75,7 @@ public class EmailSMTPRepository implements EmailRepository {
 				"                                <tr>\n" +
 				"                                    <td style=\"padding:0 35px;\">\n" +
 				"                                        <h1 style=\"color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;\">Te recordamos\n" +
-				"                                            tu contraseña del sistema de  rutas de la Universidad Manuela Beltran</h1>\n" +
+				"                                            tu contraseña del sistema de  cppm de la Andes</h1>\n" +
 				"                                        <span\n" +
 				"                                            style=\"display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100%;\"></span>\n" +
 				"                                        <p style=\"color:#455056; font-size:15px;line-height:24px; margin:0;\">\n" +
