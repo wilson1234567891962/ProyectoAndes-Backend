ef8b9d28ab63856b98346f25c11c2fb5ad01cc4a
wilson.gonzalez <wilson.gonzalez.ext@itau.co>
2023-03-17 17:28:31 -0500

    Added one test to check the jwt when it is valid

--- src/test/java/com/co/andes/management/utils/UtilsTest.java
+++ src/test/java/com/co/andes/management/utils/UtilsTest.java
@@ -1,6 +1,7 @@
 package com.co.andes.management.utils;
 
 import com.co.andes.management.utils.exception.AndesException;
+import com.co.andes.management.utils.token.JwtUtils;
 import org.junit.Test;
 
 public class UtilsTest {
@@ -8,5 +9,10 @@ public class UtilsTest {
     @Test(expected = AndesException.class)
     public void checkJWTWithException() throws AndesException {
         Utils.checkToken("mkyong");
+    }
+
+    @Test()
+    public void checkTheJWTWhenItIsValid() throws AndesException {
+        Utils.checkToken("eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzkwOTIwNzQsInN1YiI6IiIsImV4cCI6MTY3OTIxMjA3NH0.2QsarS_RcGN_Y4-2MEr0POMFZ0SgiO5-OMsAuP0-yR8");
     }
 }
