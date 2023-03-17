4538f7b4b03ff8be4b447628cfa36c4c7e0de562
wilson.gonzalez <wilson.gonzalez.ext@itau.co>
2023-03-17 17:12:15 -0500

    Fix the jwt test

--- src/test/java/com/co/andes/management/utils/UtilsTest.java
+++ src/test/java/com/co/andes/management/utils/UtilsTest.java
@@ -2,11 +2,10 @@ package com.co.andes.management.utils;
 
 import com.co.andes.management.utils.exception.AndesException;
 import org.junit.Test;
-import org.mockito.internal.matchers.Null;
 
 public class UtilsTest {
 
-    @Test(expected = NullPointerException.class)
+    @Test(expected = AndesException.class)
     public void checkJWTWithException() throws AndesException {
         Utils.checkToken("mkyong");
     }
