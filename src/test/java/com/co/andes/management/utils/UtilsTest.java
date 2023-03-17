7447698aa237983fed9049b2371c7a3bbc23ddcf
wilson.gonzalez <wilson.gonzalez.ext@itau.co>
2023-03-17 17:10:46 -0500

    Fix the deployment script

--- src/test/java/com/co/andes/management/utils/UtilsTest.java
+++ src/test/java/com/co/andes/management/utils/UtilsTest.java
@@ -2,10 +2,11 @@ package com.co.andes.management.utils;
 
 import com.co.andes.management.utils.exception.AndesException;
 import org.junit.Test;
+import org.mockito.internal.matchers.Null;
 
 public class UtilsTest {
 
-    @Test(expected = AndesException.class)
+    @Test(expected = NullPointerException.class)
     public void checkJWTWithException() throws AndesException {
         Utils.checkToken("mkyong");
     }
